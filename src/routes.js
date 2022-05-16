1671bcf74c39fe32a51a5d4ecdf738840a40bdff
Aleixo Guarizo Junior <aleixo.cco@gmail.com>
2022-05-16 19:47:37 -0300

    feat(backend): Modificado páginas para ejs e configurado nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
+const { response } = require('express')
 const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (request, response) => {
-    response.render("index")
-})
+router.get('/', (request, response) =>  response.render("index"))
+router.get('/room', (request, response) => response.render("room"))
+router.get('/create-room', (request, response) => response.render("create-room"))
 
 module.exports = router;
