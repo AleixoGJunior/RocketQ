fc4144aae5f89a33b2b72d97a0e50018527e3a9b
Aleixo Guarizo Junior <aleixo.cco@gmail.com>
2022-05-18 19:47:31 -0300

    feat(room): Implementado criação de sala

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,19 @@
+const Database = require("../db/config");
+
 module.exports = {
-  create(request, response) {
-    let roomId = 323243;
+  async create(request, response) {
+    const pass = request.body.password;
+    const db = await Database();
+
+    let roomId = "";
+
+    for (var i = 0; i < 6; i++) {
+      roomId += Math.floor(Math.random() * 10).toString();
+    }
+
+    await db.run(`INSERT INTO rooms VALUES (${parseInt(roomId)}, "${pass}")`);
+
+    await db.close();
 
     response.redirect(`/room/${roomId}`);
   },
